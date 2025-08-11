806c74d03a6e43b9e7c991f1e4d7f9a9409e7481
Kevin Pirnie <me@kpirnie.com>
2025-08-11 11:13:41 +0000

    caching trait initial

---
+++ vendor/class/cache/redis.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * KPT Cache - Redis Caching Trait
+ * 
+ * @since 8.4
+ * @author Kevin Pirnie <me@kpirnie.com>
+ * @package KP Library
+ */
+
+defined( 'KPT_PATH' ) || die( 'Direct Access is not allowed!' );
+
+// make sure the trait doesn't exist first
+if( ! trait_exists( 'KPT_Cache_Redis' ) ) {
+
+    trait KPT_Cache_Redis {
+
+    }
+
+}
+        
---
+++ vendor/class/cache/yac.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * KPT Cache - YAC Caching Trait
+ * 
+ * @since 8.4
+ * @author Kevin Pirnie <me@kpirnie.com>
+ * @package KP Library
+ */
+
+defined( 'KPT_PATH' ) || die( 'Direct Access is not allowed!' );
+
+// make sure the trait doesn't exist first
+if( ! trait_exists( 'KPT_Cache_YAC' ) ) {
+
+    trait KPT_Cache_YAC {
+
+    }
+
+}
+        
